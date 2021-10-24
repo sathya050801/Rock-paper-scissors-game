dbc120e806ee26a8e96c21c9621e729703305dac
T Sathya Narayana <61154612+sathya050801@users.noreply.github.com>
2021-10-24 22:38:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+let userScore= 0;
+let computerScore= 0;
+const userScore_span= document.getElementById("user-score");
+const computerScore_span= document.getElementById("computer-score");
+const scoreBoard_div=document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div=document.getElementById("r");
+const paper_div=document.getElementById("p");
+const scissors_div=document.getElementById("s");
+const smallUserWord = "user".fontsize(3).fontcolor("orange").sub();
+const smallCompWord = "computer".fontsize(3).fontcolor("orange").sub();
+
+function getComputerChoice()
+{
+    const choices= ['r','p','s'];
+    const randomNumber= Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter)
+{
+    if(letter === "r") return "Rock";
+    if(letter === "p") return "Paper";
+    if(letter === "s") return "Scissors";
+}
+
+ function win(userChoice , computerChoice)
+ {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = convertToWord(userChoice)+" "+smallUserWord+" beats " + convertToWord(computerChoice)+" "+smallCompWord+ " . You win :)";
+  document.getElementById(userChoice).classList.add("green-glow");
+  setTimeout(() => { document.getElementById(userChoice).classList.remove('green-glow')}, 500);
+ }
+
+ function lose(userChoice, computerChoice)
+ {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  userScore_span.innerHTML = userScore;
+  result_p.innerHTML = convertToWord(computerChoice)+" "+smallCompWord+" beats " + convertToWord(userChoice)+" "+smallUserWord+ " . You lost :(";
+  document.getElementById(userChoice).classList.add("red-glow");
+  setTimeout( () => { document.getElementById(userChoice).classList.remove('red-glow')}, 500);
+ }
+
+ function draw(userChoice , computerChoice)
+ {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(computerChoice)+" "+smallCompWord+" equals " + convertToWord(userChoice)+" "+smallUserWord+ " . It's a draw";
+    document.getElementById(userChoice).classList.add("grey-glow");
+    setTimeout( () => { document.getElementById(userChoice).classList.remove('grey-glow')}, 500);
+ }
+
+
+function game(userChoice)
+{
+const computerChoice = getComputerChoice();
+switch(userChoice + computerChoice)
+{
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+    break;
+    
+    case "sr":
+    case "rp":
+    case "ps":
+        lose(userChoice, computerChoice);
+    break;
+
+    case "rr":
+    case "pp":
+    case "ss":
+        draw(userChoice, computerChoice);
+    break;
+}
+}
+
+function main()
+{
+   rock_div.addEventListener('click',() => game("r"));
+
+   paper_div.addEventListener('click', ()=> game("p"))
+
+   scissors_div.addEventListener('click', ()=> game("s"));
+}
+
+main();
